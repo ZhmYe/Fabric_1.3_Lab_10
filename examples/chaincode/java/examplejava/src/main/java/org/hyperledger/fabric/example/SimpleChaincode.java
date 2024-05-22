e81e2bd5bdccbd75208f0dd5b691100e896eb19c
ZhmYe <zhmye@stu.ecnu.edu.cn>
2024-05-22 14:32:45 +0800

    init

---
+++ examples/chaincode/java/examplejava/src/main/java/org/hyperledger/fabric/example/SimpleChaincode.java
@@ -0,0 +1,87 @@
+package org.hyperledger.fabric.example;
+
+import java.util.List;
+
+import com.google.protobuf.ByteString;
+import io.netty.handler.ssl.OpenSsl;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hyperledger.fabric.shim.ChaincodeBase;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class SimpleChaincode extends ChaincodeBase {
+
+    private static Log _logger = LogFactory.getLog(SimpleChaincode.class);
+
+    @Override
+    public Response init(ChaincodeStub stub) {
+        try {
+            _logger.info("Init java simple chaincode");
+            String func = stub.getFunction();
+            if (!func.equals("init")) {
+                return newErrorResponse("function other than init is not supported");
+            }
+            List<String> args = stub.getParameters();
+            if (args.size() != 4) {
+                newErrorResponse("Incorrect number of arguments. Expecting 4");
+            }
+            // Initialize the chaincode
+            String account1Key = args.get(0);
+            int account1Value = Integer.parseInt(args.get(1));
+            String account2Key = args.get(2);
+            int account2Value = Integer.parseInt(args.get(3));
+
+            _logger.info(String.format("account %s, value = %s; account %s, value %s", account1Key, account1Value, account2Key, account2Value));
+            stub.putStringState(account1Key, args.get(1));
+            stub.putStringState(account2Key, args.get(3));
+
+            return newSuccessResponse();
+        } catch (Throwable e) {
+            return newErrorResponse(e);
+        }
+    }
+
+    @Override
+    public Response invoke(ChaincodeStub stub) {
+        try {
+            _logger.info("Invoke java simple chaincode");
+            String func = stub.getFunction();
+            List<String> params = stub.getParameters();
+            if (func.equals("invoke")) {
+                return invoke(stub, params);
+            }
+            if (func.equals("delete")) {
+                return delete(stub, params);
+            }
+            if (func.equals("query")) {
+                return query(stub, params);
+            }
+            return newErrorResponse("Invalid invoke function name. Expecting one of: [\"invoke\", \"delete\", \"query\"]");
+        } catch (Throwable e) {
+            return newErrorResponse(e);
+        }
+    }
+
+    private Response invoke(ChaincodeStub stub, List<String> args) {
+        //todo
+    }
+
+    // Deletes an entity from state
+    private Response delete(ChaincodeStub stub, List<String> args) {
+        //todo
+    }
+
+    // query callback representing the query of a chaincode
+    private Response query(ChaincodeStub stub, List<String> args) {
+        //todo
+    }
+
+    public static void main(String[] args) {
+        System.out.println("OpenSSL avaliable: " + OpenSsl.isAvailable());
+        new SimpleChaincode().start(args);
+    }
+
+}
+
